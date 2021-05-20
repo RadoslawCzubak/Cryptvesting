27dce9b40a9f88d68032a85fbafb4f2c1e392d6c
Radoslaw Czubak <radoslaw.marek.czubak@gmail.com>
2021-05-20 22:33:07 +0200

    add room converters

---
+++ app/src/main/java/com/rczubak/cryptvesting/utils/roomUtils/Converters.kt
@@ -0,0 +1,19 @@
+package com.rczubak.cryptvesting.utils.roomUtils
+
+import androidx.room.TypeConverter
+import org.threeten.bp.LocalDateTime
+import org.threeten.bp.format.DateTimeFormatter
+
+class Converters {
+    @TypeConverter
+    fun fromDatabaseDate(value: String): LocalDateTime {
+        val dateFormatter = DateTimeFormatter.ISO_DATE_TIME
+        return LocalDateTime.parse(value, dateFormatter)
+    }
+
+    @TypeConverter
+    fun toDatabaseDate(value: LocalDateTime): String {
+        val dateFormatter = DateTimeFormatter.ISO_DATE_TIME
+        return value.format(dateFormatter)
+    }
+}
