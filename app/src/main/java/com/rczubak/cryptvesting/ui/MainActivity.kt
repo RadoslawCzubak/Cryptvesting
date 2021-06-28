0f65f5154de554aa0bbd2bfc37deb281e60f834a
Radosław Czubak <56758365+RadoslawCzubak@users.noreply.github.com>
2021-06-28 20:36:33 +0200

    Create dashboard view (#3)
    * add databinding feature
    
    * add navigation
    
    * add dashboard with balance ui
    
    * add wallet cardview

--- app/src/main/java/com/rczubak/cryptvesting/ui/MainActivity.kt
+++ app/src/main/java/com/rczubak/cryptvesting/ui/MainActivity.kt
@@ -3,10 +3,20 @@ package com.rczubak.cryptvesting.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rczubak.cryptvesting.R
+import com.rczubak.cryptvesting.utils.TransactionCalculator
+import com.rczubak.cryptvesting.utils.XlsReader
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import java.io.InputStream
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
    }
+
+    override fun onStart() {
+        super.onStart()
+    }
 }
--- app/src/main/java/com/rczubak/cryptvesting/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/rczubak/cryptvesting/ui/dashboard/DashboardFragment.kt
@@ -1,19 +1,24 @@
 package com.rczubak.cryptvesting.ui.dashboard
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.rczubak.cryptvesting.R
+import com.rczubak.cryptvesting.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
+
+    lateinit var binding: FragmentDashboardBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_dashboard, container, false)
+        return binding.root
     }
 }
