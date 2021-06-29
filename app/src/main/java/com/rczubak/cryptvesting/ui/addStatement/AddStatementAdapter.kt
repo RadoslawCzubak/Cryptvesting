05a2132b39d9e23c07cc01e4e1543a81a783fbd4
Radoslaw Czubak <radoslaw.marek.czubak@gmail.com>
2021-06-29 21:07:40 +0200

    add fragment, viewmodel, adapter and layouts

---
+++ app/src/main/java/com/rczubak/cryptvesting/ui/addStatement/AddStatementAdapter.kt
@@ -0,0 +1,40 @@
+package com.rczubak.cryptvesting.ui.addStatement
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.rczubak.cryptvesting.data.models.domain.CryptoCurrencyModel
+import com.rczubak.cryptvesting.databinding.ItemTransactionBinding
+
+class AddStatementAdapter(private val onRecyclerViewEmpty: () -> Unit) : RecyclerView.Adapter<AddStatementAdapter.AddStatementViewHolder>() {
+    inner class AddStatementViewHolder(private val binding: ItemTransactionBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(cryptoCurrencyModel: CryptoCurrencyModel){
+            binding.crypto = cryptoCurrencyModel
+        }
+    }
+
+    private val data = ArrayList<CryptoCurrencyModel>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddStatementViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemTransactionBinding.inflate(inflater)
+        return AddStatementViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: AddStatementViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        if (data.size == 0){
+            onRecyclerViewEmpty()
+        }
+        return data.size
+    }
+
+    fun updateData(data: ArrayList<CryptoCurrencyModel>) {
+        this.data.clear()
+        this.data.addAll(data)
+    }
+}
---
+++ app/src/main/java/com/rczubak/cryptvesting/ui/addStatement/AddStatementViewModel.kt
@@ -0,0 +1,7 @@
+package com.rczubak.cryptvesting.ui.addStatement
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class AddStatementViewModel @Inject constructor() : ViewModel() {
+}
