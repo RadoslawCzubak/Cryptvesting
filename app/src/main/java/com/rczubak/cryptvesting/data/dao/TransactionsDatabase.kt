f5954d5e4c2c1abe25e2e640c01aaa28551b9e62
Radoslaw Czubak <radoslaw.marek.czubak@gmail.com>
2021-08-24 20:49:06 +0200

    fix dagger

---
+++ app/src/main/java/com/rczubak/cryptvesting/data/dao/TransactionsDatabase.kt
@@ -0,0 +1,10 @@
+package com.rczubak.cryptvesting.data.dao
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.rczubak.cryptvesting.data.models.entities.TransactionEntity
+
+@Database(entities = [TransactionEntity::class], version = 1)
+abstract class TransactionsDatabase: RoomDatabase() {
+    abstract fun transactionDao(): TransactionsDao
+}
---
+++ app/src/main/java/com/rczubak/cryptvesting/di/modules/AppModule.kt
@@ -0,0 +1,25 @@
+package com.rczubak.cryptvesting.di.modules
+
+import android.content.Context
+import androidx.room.Room
+import com.rczubak.cryptvesting.data.dao.TransactionsDao
+import com.rczubak.cryptvesting.data.dao.TransactionsDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+final class AppModule {
+    @Provides
+    fun provideTransactionsDao(@ApplicationContext applicationContext: Context): TransactionsDao {
+        val db = Room.databaseBuilder(
+            applicationContext,
+            TransactionsDatabase::class.java, "database-name"
+        ).build()
+
+        return db.transactionDao()
+    }
+}
