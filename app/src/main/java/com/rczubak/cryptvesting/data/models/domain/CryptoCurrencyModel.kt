9338d4eb2352b9c144434dbc630e94bac94276d4
Radoslaw Czubak <radoslaw.marek.czubak@gmail.com>
2021-08-26 22:56:09 +0200

    update cryptocurrencymodel

--- app/src/main/java/com/rczubak/cryptvesting/data/models/domain/CryptoCurrencyModel.kt
+++ app/src/main/java/com/rczubak/cryptvesting/data/models/domain/CryptoCurrencyModel.kt
@@ -1,5 +1,6 @@
 package com.rczubak.cryptvesting.data.models.domain
 
+import com.rczubak.cryptvesting.data.models.entities.CryptocurrencyEntity
 import org.threeten.bp.LocalDateTime
 
 data class CryptoCurrencyModel(
@@ -8,4 +9,12 @@ data class CryptoCurrencyModel(
     val price: Double,
     val priceDate: LocalDateTime,
     val priceCurrency: String
-)
+) {
+    constructor(crypto: CryptocurrencyEntity) : this(
+        crypto.name,
+        crypto.symbol,
+        crypto.price,
+        crypto.priceDate,
+        crypto.priceCurrency
+    )
+}
