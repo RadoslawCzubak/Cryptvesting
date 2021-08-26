d41fbe0ab988626f00cc3897491ad3ead8da035e
Radoslaw Czubak <radoslaw.marek.czubak@gmail.com>
2021-08-26 23:50:52 +0200

    add converter to entity

--- app/src/main/java/com/rczubak/cryptvesting/data/models/entities/CryptocurrencyEntity.kt
+++ app/src/main/java/com/rczubak/cryptvesting/data/models/entities/CryptocurrencyEntity.kt
@@ -2,10 +2,13 @@ package com.rczubak.cryptvesting.data.models.entities
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
 import com.rczubak.cryptvesting.data.models.domain.CryptoCurrencyModel
+import com.rczubak.cryptvesting.utils.roomUtils.Converters
 import org.threeten.bp.LocalDateTime
 
 @Entity(tableName = "cryptocurrencies")
+@TypeConverters(Converters::class)
 data class CryptocurrencyEntity(
     val name: String,
     val symbol: String,
