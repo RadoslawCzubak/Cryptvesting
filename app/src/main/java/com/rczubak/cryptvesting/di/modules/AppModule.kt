84f68d19d17019c4ac5bc7ea38d7737f6998c6e7
Radoslaw Czubak <radoslaw.marek.czubak@gmail.com>
2021-08-26 23:46:55 +0200

    update dependencies

--- app/src/main/java/com/rczubak/cryptvesting/di/modules/AppModule.kt
+++ app/src/main/java/com/rczubak/cryptvesting/di/modules/AppModule.kt
@@ -2,13 +2,22 @@ package com.rczubak.cryptvesting.di.modules
 
 import android.content.Context
 import androidx.room.Room
+import com.rczubak.cryptvesting.BuildConfig
+import com.rczubak.cryptvesting.data.dao.cryptocurrencyinfo.CryptocurrenciesDao
+import com.rczubak.cryptvesting.data.dao.cryptocurrencyinfo.CryptocurrenciesDatabase
 import com.rczubak.cryptvesting.data.dao.transactions.TransactionsDao
 import com.rczubak.cryptvesting.data.dao.transactions.TransactionsDatabase
+import com.rczubak.cryptvesting.data.network.ApiKeyInterceptor
+import com.rczubak.cryptvesting.data.network.services.NomicsApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -21,5 +30,33 @@ final class AppModule {
         ).build()
 
         return db.transactionDao()
+    }
+
+    @Provides
+    fun provideCryptoCurrenciesDao(@ApplicationContext context: Context): CryptocurrenciesDao {
+        val db = Room.databaseBuilder(
+            context,
+            CryptocurrenciesDatabase::class.java, "cryptocurrencies-state-db",
+        ).build()
+
+        return db.dao()
+    }
+
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        return OkHttpClient.Builder()
+            .addInterceptor(ApiKeyInterceptor())
+            .addInterceptor(loggingInterceptor).build()
+    }
+
+    @Provides
+    fun provideNomicsApi(client: OkHttpClient): NomicsApi {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.NOMICS_API_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(NomicsApi::class.java)
     }
 }
